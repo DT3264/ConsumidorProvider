b9c8ac49a978b8bc1c7b5fe0e92278ca0bb17e1c
DT3264 <cernadaniel32@gmail.com>
2022-02-05 11:35:56 -0600

    Initial commit

---
+++ app/src/main/java/com/dcerna/consumidorprovider/MainActivity.java
@@ -0,0 +1,90 @@
+package com.dcerna.consumidorprovider;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnInsertar;
+    Button btnLeer;
+    Button btnActualiza;
+    TextView txt;
+    EditText txtID, txtNombre, txtApellido;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt = findViewById(R.id.txt);
+        txtID = findViewById(R.id.txtID);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtApellido = findViewById(R.id.txtApellido);
+        btnLeer = findViewById(R.id.btnLeer);
+        btnInsertar = findViewById(R.id.btnInsertar);
+        btnActualiza = findViewById(R.id.btnActualiza);
+
+        btnInsertar.setOnClickListener(view -> {
+            insertaValor();
+        });
+        btnLeer.setOnClickListener(view -> {
+            leeValores();
+        });
+        btnActualiza.setOnClickListener(view -> {
+            actualizaDatos();
+        });
+    }
+
+    private void actualizaDatos() {
+        String id = txtID.getText().toString();
+        String nombre = txtNombre.getText().toString();
+        String apellido = txtApellido.getText().toString();
+
+        ContentValues valores = new ContentValues();
+        valores.put(UsuarioContrato.COLUMN_FIRST_NAME, nombre);
+        valores.put(UsuarioContrato.COLUMN_LAST_NAME, apellido);
+
+        Uri nuevaUri = Uri.withAppendedPath(UsuarioContrato.CONTENT_URI, id);
+        int resultado = getContentResolver().update(nuevaUri, valores, null, null);
+        txt.setText("Actualizado?: " + resultado);
+    }
+
+    private void leeValores() {
+        StringBuilder builder = new StringBuilder();
+        Uri contentUri = UsuarioContrato.CONTENT_URI;
+        String[] columnas = UsuarioContrato.COLUMNS;
+        Cursor cursor = getContentResolver().query(contentUri, columnas,
+                null, null, null);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                String s0 = cursor.getString(0);
+                String s1 = cursor.getString(1);
+                String s2 = cursor.getString(2);
+                String datos = String.format("%s-%s-%s", s0, s1, s2);
+                builder.append(datos);
+                builder.append('\n');
+            }
+            Log.d("MainActivity", builder.toString());
+            txt.setText(builder.toString());
+        } else {
+            Log.d("MainActivity", "Cursor nulo");
+            txt.setText("Cursor nulo");
+        }
+    }
+
+    private void insertaValor() {
+        ContentValues valores = new ContentValues();
+        valores.put(UsuarioContrato.COLUMN_FIRST_NAME, "Pedro");
+        valores.put(UsuarioContrato.COLUMN_LAST_NAME, "Paramo");
+        Uri nuevoUsuario = getContentResolver().insert(UsuarioContrato.CONTENT_URI, valores);
+        txt.setText("Insertado: " + nuevoUsuario.toString());
+    }
+}
